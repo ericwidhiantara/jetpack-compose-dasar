4c21186af0051b2ac58d8ae3ae97c681014442c8
eric <nukenin.konoha@gmail.com>
2024-07-29 17:01:30 +0700

    feature: add annotated string text example

--- app/src/main/java/com/example/myapplication/TextExample.kt
+++ app/src/main/java/com/example/myapplication/TextExample.kt
@@ -10,23 +10,42 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun TextExample(modifier: Modifier = Modifier, text: String = "") {
+    val annotated = buildAnnotatedString {
+        withStyle(MaterialTheme.typography.bodyMedium.toSpanStyle()) {
+            append("Komang, ")
+        }
+        append("Eric ")
+        withStyle(
+            MaterialTheme.typography.bodyMedium.toSpanStyle().copy(fontWeight = FontWeight.Bold)
+        ) {
+            append("Widhi ")
+        }
+        withStyle(MaterialTheme.typography.bodyMedium.toSpanStyle()) {
+            append("Antara")
+        }
+    }
+
+
+
     Text(
-        text = text,
+        text = "$annotated $text",
         modifier = modifier
             .background(Color.Green, shape = RoundedCornerShape(12.dp))
             .padding(16.dp)
-            .clickable { Log.d("lol","clicked" ) },
+            .clickable { Log.d("lol", "clicked") },
         color = Color.Blue,
         fontSize = 24.sp,
         fontStyle = MaterialTheme.typography.bodyMedium.fontStyle,
