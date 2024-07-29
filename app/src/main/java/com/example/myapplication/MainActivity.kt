ca6378ab71c8c6816706150ddfa20defb3255c20
eric <nukenin.konoha@gmail.com>
2024-07-29 19:22:48 +0700

    feature: add outlined text field example

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TextFieldExample(
+                    OutlinedTextFieldExample(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
