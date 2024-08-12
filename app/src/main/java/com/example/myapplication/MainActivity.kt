33697d8310b0b27ade1486dfb86a7bdbe3cf2902
eric <nukenin.konoha@gmail.com>
2024-08-12 16:06:57 +0700

    feature: add linear progress indicator example

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
                         TopAppBar(title = { Text("My App") })
                     }
                 ) { innerPadding ->
-                    CircularProgressIndicatorExample(
+                    LinearProgressIndicatorExample(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
